bb10946df0a48b5e06698e43ed9212e277e1b0df
Meltan255 <134165155+Meltan255@users.noreply.github.com>
2024-06-27 18:14:54 -0700

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 700.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
